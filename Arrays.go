e78e6476e7228b9f77a3a1ba4e87f13b4417a9f8
97631431+kkassim1@users.noreply.github.com <97631431+kkassim1@users.noreply.github.com>
2022-03-09 13:54:30 -0700

    testing git

--- Arrays.go
+++ Arrays.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
+func TwoS(array []int, target int) []int {
+
+	for i := 0; i < len(array); i++ {
+		fmt.Println(array[i] + 1)
+	}
+
+	return array
+
+}
 func main() {
 
-	fmt.Println("kkhvjhghvg")
+	var test = []int{7, 8, 3}
+
+	t := TwoS(test, 3)
+
+	// fmt.Println("kkjhghvg", append(t, test...))
+	fmt.Println("kkjhghvg", t)
 }
