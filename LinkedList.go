33d03b3ab36f9e8b7198e53947a2147a598bdb1d
97631431+kkassim1@users.noreply.github.com <97631431+kkassim1@users.noreply.github.com>
2022-03-16 20:13:52 -0700

    linkedList insert in begginging  implementation  --changed from Array.go to LinkedList

--- LinkedList.go
+++ LinkedList.go
@@ -4,7 +4,48 @@ import (
 	"fmt"
 )
 
-func mains() {
+// linkedlist
 
-	fmt.Println("gf")
+type Node struct {
+	next *Node
+	data int
+}
+
+type LinkedList struct {
+	len  int
+	head *Node
+}
+
+func (list *LinkedList) insertBeg(data int) {
+
+	tempNode := Node{data: data}
+
+	tempNode.data = data
+	tempNode.next = list.head
+	list.head = &tempNode
+	// return
+}
+
+func (list *LinkedList) prints() {
+	if list.head == nil {
+		return
+	}
+
+	temp := list.head
+
+	for temp != nil {
+		fmt.Println(temp.data)
+		temp = temp.next
+	}
+
+}
+
+func main() {
+
+	t := LinkedList{}
+
+	t.insertBeg(4)
+	t.insertBeg(7)
+
+	t.prints()
 }
